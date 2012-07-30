6c01c84e3dad24bf9f4895c4fe5f5715b3aaf863
John Gietzen <otac0n@gietzen.us>
2012-07-29 20:51:24 -0700

    Change `Pegasus` to an executable and added a rudimentary compilation procedure.

---
+++ Pegasus/Program.cs
@@ -0,0 +1,26 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Program.cs" company="(none)">
+//   Copyright © 2012 John Gietzen.  All Rights Reserved.
+//   This source is subject to the MIT license.
+//   Please see license.txt for more information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Pegasus
+{
+    using System.IO;
+    using Pegasus.Compiler;
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = File.ReadAllText(args[0]);
+            var parser = new PegParser();
+            var grammar = parser.Parse(input);
+            var compiler = new PegCompiler();
+            var result = compiler.Compile(grammar.Value);
+            File.WriteAllText(Path.GetFileNameWithoutExtension(args[0]) + ".cs", result.Code);
+        }
+    }
+}
