80cbfa9d3dc8a23e6d441cbea09eaf9b1b18a296
John Gietzen <otac0n@gietzen.us>
2012-08-23 19:59:04 -0700

    Moved the logic for compiling grammars into the new `CompileManager` class.

---
+++ Pegasus/CompileManager.cs
@@ -0,0 +1,42 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="CompileManager.cs" company="(none)">
+//   Copyright © 2012 John Gietzen.  All Rights Reserved.
+//   This source is subject to the MIT license.
+//   Please see license.txt for more information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Pegasus
+{
+    using System;
+    using System.CodeDom.Compiler;
+    using System.IO;
+    using Pegasus.Compiler;
+    using Pegasus.Parser;
+
+    internal static class CompileManager
+    {
+        public static void CompileFile(string inputFile, string outputFile, Action<CompilerError> logError)
+        {
+            outputFile = outputFile ?? inputFile + ".cs";
+
+            var subject = File.ReadAllText(inputFile);
+            var parser = new PegParser();
+            var grammar = parser.Parse(subject);
+            var compiler = new PegCompiler();
+            var result = compiler.Compile(grammar);
+
+            bool hadFatal = false;
+            foreach (var error in result.Errors)
+            {
+                hadFatal |= !error.IsWarning;
+                logError(error);
+            }
+
+            if (!hadFatal)
+            {
+                File.WriteAllText(outputFile, result.Code);
+            }
+        }
+    }
+}
--- Pegasus/Program.cs
+++ Pegasus/Program.cs
@@ -9,29 +9,12 @@
 namespace Pegasus
 {
     using System;
-    using System.IO;
-    using System.Linq;
-    using Pegasus.Compiler;
-    using Pegasus.Parser;
 
     internal class Program
     {
         public static void Main(string[] args)
         {
-            var input = File.ReadAllText(args[0]);
-            var parser = new PegParser();
-            var grammar = parser.Parse(input);
-            var compiler = new PegCompiler();
-            var result = compiler.Compile(grammar);
-            foreach (var error in result.Errors)
-            {
-                Console.Error.WriteLine(error.ToString());
-            }
-
-            if (!result.Errors.Any(e => !e.IsWarning))
-            {
-                File.WriteAllText(args[0] + ".cs", result.Code);
-            }
+            CompileManager.CompileFile(args[0], null, Console.WriteLine);
         }
     }
 }
