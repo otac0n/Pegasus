5b1f36e6bf202fa3673dc87c64808c8b93b587e6
John Gietzen <otac0n@gietzen.us>
2013-01-14 13:06:24 -0800

    Added equality implementation for `CharacterRange`.

--- Pegasus/Expressions/CharacterRange.cs
+++ Pegasus/Expressions/CharacterRange.cs
@@ -8,10 +8,12 @@
 
 namespace Pegasus.Expressions
 {
+    using System;
+
     /// <summary>
     /// Represents an inclusive range of characters.
     /// </summary>
-    public class CharacterRange
+    public class CharacterRange : IEquatable<CharacterRange>
     {
         private readonly char max;
         private readonly char min;
@@ -41,6 +43,41 @@ namespace Pegasus.Expressions
         public char Min
         {
             get { return this.min; }
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object"/> is equal to the current <see cref="CharacterRange"/>.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object"/> to compare with the current <see cref="CharacterRange"/>.</param>
+        /// <returns>true if the specified <see cref="System.Object"/> is equal to the current <see cref="CharacterRange"/>; otherwise, false.</returns>
+        public override bool Equals(object obj)
+        {
+            return this.Equals(obj as CharacterRange);
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="CharacterRange"/> is equal to the current <see cref="CharacterRange"/>.
+        /// </summary>
+        /// <param name="other">The <see cref="CharacterRange"/> to compare with the current <see cref="CharacterRange"/>.</param>
+        /// <returns>true if the specified <see cref="CharacterRange"/> is equal to the current <see cref="CharacterRange"/>; otherwise, false.</returns>
+        public bool Equals(CharacterRange other)
+        {
+            return
+                other != null &&
+                other.min == this.min &&
+                other.max == this.max;
+        }
+
+        /// <summary>
+        /// Returns the hash code for this instance.
+        /// </summary>
+        /// <returns>A 32-bit signed integer hash code.</returns>
+        public override int GetHashCode()
+        {
+            int hash = 0x51ED270B;
+            hash = (hash * -0x25555529) + this.min.GetHashCode();
+            hash = (hash * -0x25555529) + this.max.GetHashCode();
+            return hash;
         }
     }
 }
