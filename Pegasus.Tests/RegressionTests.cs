e077ca677632e7825918afb7bf80805847ea4c9a
John Gietzen <otac0n@gietzen.us>
2013-03-18 22:24:12 -0700

    Added regression test for fixing bug #20.

---
+++ Pegasus.Tests/RegressionTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Pegasus.Tests
+{
+    using NUnit.Framework;
+    using Pegasus.Compiler;
+    using Pegasus.Parser;
+
+    [TestFixture]
+    public class RegressionTests
+    {
+        [Test(Description = "GitHub bug #20")]
+        public void Compile_WhenGivenAGrammarWithARuleWithAnImmediateTypedExpression_DoesNotThrowAnExceptionOrReturnErrors()
+        {
+            var grammar = new PegParser().Parse("top = item:(<string> 'a' 'b') {item}");
+
+            var result = PegCompiler.Compile(grammar);
+            Assert.That(result.Errors, Is.Empty);
+        }
+    }
+}
