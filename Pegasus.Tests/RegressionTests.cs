d5c78771ecde0713a9f1b825b4c1b5f03c047b6b
John Gietzen <otac0n@gietzen.us>
2013-03-18 22:33:34 -0700

    Clean up.

--- Pegasus.Tests/RegressionTests.cs
+++ Pegasus.Tests/RegressionTests.cs
@@ -1,4 +1,12 @@
-﻿namespace Pegasus.Tests
+﻿// -----------------------------------------------------------------------
+// <copyright file="RegressionTests.cs" company="(none)">
+//   Copyright © 2013 John Gietzen.  All Rights Reserved.
+//   This source is subject to the MIT license.
+//   Please see license.txt for more information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Pegasus.Tests
 {
     using System.Linq;
     using NUnit.Framework;
