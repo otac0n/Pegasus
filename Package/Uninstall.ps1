4a61c41d365bd4f000ad76daf1c94962859cd493
John Gietzen <otac0n@gietzen.us>
2013-06-23 12:34:28 -0700

    Updated the package to take advantage of the MSBuild integation available in NuGet 2.5.

--- Package/Uninstall.ps1
+++
@@ -1,8 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-Add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
-$msbuild = [Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($project.FullName) | Select-Object -First 1
-$msbuild.Xml.Imports | Where-Object {$_.Project.ToLowerInvariant().EndsWith("pegasus.targets") } | Foreach {
-    write-host "Removing Pegasus Targets File:" $_.Project
-    $_.Parent.RemoveChild($_)
-}
