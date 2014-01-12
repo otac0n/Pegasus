773af4ccd30bfcdc0baffa6920d47dab1e73e1f9
John Gietzen <otac0n@gietzen.us>
2014-01-12 00:01:17 -0800

    Made debugging easier.

--- Pegasus.Common/LexicalElement.cs
+++ Pegasus.Common/LexicalElement.cs
@@ -8,11 +8,19 @@
 
 namespace Pegasus.Common
 {
+    using System.Diagnostics;
+
     /// <summary>
     /// A basic lexical element class that marks a region of text with a given name.
     /// </summary>
+    [DebuggerDisplay("{Name}@{StartCursor.Location}:{EndCursor.Location}")]
     public class LexicalElement : ILexical
     {
+        /// <summary>
+        /// Gets or sets the ending cursor of this instance.
+        /// </summary>
+        public Cursor EndCursor { get; set; }
+
         /// <summary>
         /// Gets or sets the name associated with the region of text.
         /// </summary>
@@ -22,10 +30,5 @@ namespace Pegasus.Common
         /// Gets or sets the starting cursor of this instance.
         /// </summary>
         public Cursor StartCursor { get; set; }
-
-        /// <summary>
-        /// Gets or sets the ending cursor of this instance.
-        /// </summary>
-        public Cursor EndCursor { get; set; }
     }
 }
