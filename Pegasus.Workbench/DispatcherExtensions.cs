2bd88a924f187f3e8d187e30affeae7bc01cea52
John Gietzen <otac0n@gietzen.us>
2014-01-13 18:50:35 -0800

    Added focusing the text editor when an error is double-clicked.

---
+++ Pegasus.Workbench/DispatcherExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Pegasus.Workbench
+{
+    using System;
+    using System.Windows.Threading;
+
+    internal static class DispatcherExtensions
+    {
+        public static DispatcherOperation BeginInvoke(this Dispatcher dispatcher, Action action)
+        {
+            return dispatcher.BeginInvoke(action, null);
+        }
+    }
+}
--- Pegasus.Workbench/MainWindow.xaml.cs
+++ Pegasus.Workbench/MainWindow.xaml.cs
@@ -8,7 +8,10 @@
 
 namespace Pegasus.Workbench
 {
+    using System.CodeDom.Compiler;
     using System.Windows;
+    using System.Windows.Controls;
+    using ICSharpCode.TextEditor;
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -68,5 +71,40 @@ namespace Pegasus.Workbench
         }
 
         public AppViewModel ViewModel { get; protected set; }
+
+        public void ErrorRow_DoubleClick(object sender, RoutedEventArgs e)
+        {
+            var row = sender as DataGridRow;
+            var item = row.Item as CompilerError;
+
+            FocusError(item);
+        }
+
+        private void FocusError(CompilerError error)
+        {
+            TextEditorControl target;
+            TabItem tab;
+            if (error.FileName == this.ViewModel.GrammarFileName)
+            {
+                target = this.GrammarEditor;
+                tab = this.GrammarTab;
+            }
+            else if (error.FileName == this.ViewModel.TestFileName)
+            {
+                target = this.TestEditor;
+                tab = this.TestTab;
+            }
+            else
+            {
+                return;
+            }
+
+            tab.IsSelected = true;
+            UpdateLayout();
+
+            target.ActiveTextAreaControl.SelectionManager.ClearSelection();
+            target.ActiveTextAreaControl.Caret.Position = new TextLocation(error.Column - 1, error.Line - 1);
+            target.Focus();
+        }
     }
 }
