d1b7f4bf6fbdb253d330f5722082b88179b88d05
John Gietzen <otac0n@gietzen.us>
2014-01-18 22:29:21 -0800

    Added Roslyn integration, solving almost all of the woes related to code sections. Fixes #26, #33.

--- Pegasus/Compiler/GenerateCodePass.cs
+++ Pegasus/Compiler/GenerateCodePass.cs
@@ -11,13 +11,14 @@ namespace Pegasus.Compiler
     using System.Collections.Generic;
     using System.Globalization;
     using System.IO;
+    using System.Linq;
     using Pegasus.Expressions;
 
     internal class GenerateCodePass : CompilePass
     {
         public override IList<string> BlockedByErrors
         {
-            get { return new[] { "PEG0001", "PEG0002", "PEG0003", "PEG0005", "PEG0007", "PEG0012", "PEG0016", "PEG0019", "PEG0020", "PEG0021" }; }
+            get { return new[] { "CS0000", "PEG0001", "PEG0002", "PEG0003", "PEG0005", "PEG0007", "PEG0008", "PEG0012", "PEG0016", "PEG0019", "PEG0020", "PEG0021" }; }
         }
 
         public override IList<string> ErrorsProduced
@@ -27,6 +28,11 @@ namespace Pegasus.Compiler
 
         public override void Run(Grammar grammar, CompileResult result)
         {
+            if (result.Errors.Any(e => !e.IsWarning))
+            {
+                return;
+            }
+
             using (var stringWriter = new StringWriter(CultureInfo.InvariantCulture))
             {
                 new CodeGenerator(stringWriter, result.ExpressionTypes, result.LeftRecursiveRules).WalkGrammar(grammar);
