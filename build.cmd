35eb575f2cb4b4ffb5e35d416e93c3204de09196
John Gietzen <otac0n@gietzen.us>
2014-01-25 10:00:26 -0800

    Added a test runner.

--- build.cmd
+++ build.cmd
@@ -1,1 +1,4 @@
+@echo off
+.nuget\nuget install packages.config -o packages
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Pegasus.sln /p:Configuration=Release /t:Clean,Rebuild
+packages\NUnit.Runners.2.6.3\tools\nunit-console Pegasus.Tests\bin\Release\Pegasus.Tests.dll
