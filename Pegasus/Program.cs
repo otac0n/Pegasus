7e874a2f1b04ff543e02dac0c3f856406111f2cb
John Gietzen <otac0n@gietzen.us>
2014-09-21 10:01:41 -0700

    Added a usage screen and returned a meaningful error code from the EXE.

--- Pegasus/Program.cs
+++ Pegasus/Program.cs
@@ -9,12 +9,43 @@
 namespace Pegasus
 {
     using System;
+    using System.CodeDom.Compiler;
+    using System.Collections.Generic;
 
     internal class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CompileManager.CompileFile(args[0], null, Console.WriteLine);
+            if (args.Length != 1)
+            {
+                ShowUsage();
+                return -1;
+            }
+
+            var errors = new List<CompilerError>();
+            CompileManager.CompileFile(args[0], null, errors.Add);
+            ShowErrors(errors);
+
+            return errors.Count;
+        }
+
+        private static void ShowErrors(List<CompilerError> errors)
+        {
+            var startingColor = Console.ForegroundColor;
+
+            foreach (var e in errors)
+            {
+                Console.ForegroundColor = e.IsWarning ? ConsoleColor.Yellow : ConsoleColor.Red;
+                Console.WriteLine(e);
+            }
+
+            Console.ForegroundColor = startingColor;
+        }
+
+        private static void ShowUsage()
+        {
+            Console.WriteLine("Usage:");
+            Console.WriteLine("    pegasus file");
         }
     }
 }
