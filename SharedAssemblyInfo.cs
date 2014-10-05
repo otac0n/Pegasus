d84ed5940f6025c5d5ed5d3ade984f195f0009c0
John Gietzen <otac0n@gietzen.us>
2014-10-04 20:03:17 -0700

    Bumped the version number.

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -3,8 +3,8 @@ using System.Reflection;
 
 [assembly: AssemblyProduct("Pegasus")]
 [assembly: AssemblyCopyright("Copyright © 2014 John Gietzen")]
-[assembly: AssemblyVersion("3.2.0.0")]
-[assembly: AssemblyFileVersion("3.2.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
 #if !PORTABLE
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 #endif
