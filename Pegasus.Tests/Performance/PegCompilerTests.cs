f076eaeb954b744044cff1a4f87264c8f39cc78f
John Gietzen <otac0n@gietzen.us>
2014-10-10 23:28:23 -0700

    Added a couple of basic performance tests.

---
+++ Pegasus.Tests/Performance/PegCompilerTests.cs
@@ -0,0 +1,31 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PegCompilerTests.cs" company="(none)">
+//   Copyright © 2014 John Gietzen.  All Rights Reserved.
+//   This source is subject to the MIT license.
+//   Please see license.md for more information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Pegasus.Tests.Performance
+{
+    using System.IO;
+    using Pegasus.Compiler;
+    using Pegasus.Expressions;
+    using Pegasus.Parser;
+
+    public class PegCompilerTests : PerformanceTestBase
+    {
+        private readonly Grammar pegGrammar;
+
+        public PegCompilerTests()
+        {
+            this.pegGrammar = new PegParser().Parse(File.ReadAllText("PegParser.peg"));
+        }
+
+        [Evaluate]
+        public void PegGrammar()
+        {
+            PegCompiler.Compile(this.pegGrammar);
+        }
+    }
+}
---
+++ Pegasus.Tests/Performance/PegParserTests.cs
@@ -0,0 +1,29 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PegParserTests.cs" company="(none)">
+//   Copyright © 2014 John Gietzen.  All Rights Reserved.
+//   This source is subject to the MIT license.
+//   Please see license.md for more information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Pegasus.Tests.Performance
+{
+    using System.IO;
+    using Pegasus.Parser;
+
+    public class PegParserTests : PerformanceTestBase
+    {
+        private readonly string pegGrammar;
+
+        public PegParserTests()
+        {
+            this.pegGrammar = File.ReadAllText("PegParser.peg");
+        }
+
+        [Evaluate]
+        public void PegGrammar()
+        {
+            new PegParser().Parse(this.pegGrammar);
+        }
+    }
+}
