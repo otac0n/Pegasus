b733fda71787d6964b55bb69f715455cd6e15662
John Gietzen <otac0n@gietzen.us>
2016-07-20 23:06:23 -0700

    Added simple performance regression test.

---
+++ Pegasus.Tests/Performance/RegressionTests.cs
@@ -0,0 +1,32 @@
+﻿// Copyright © John Gietzen. All Rights Reserved. This source is subject to the MIT license. Please see license.md for more information.
+
+namespace Pegasus.Tests.Performance
+{
+    using System.IO;
+    using System.Linq;
+    using Compiler;
+    using NUnit.Framework;
+    using Pegasus.Parser;
+    using static PerformanceTests;
+
+    [TestFixture]
+    [Category("Performance")]
+    public class RegressionTests
+    {
+        [TestCase("simple")]
+        public void GeneratedParserPerformanceRegression(string testName)
+        {
+            var parserSource = File.ReadAllText($@"Performance\Regression\{testName}.peg");
+            var subject = File.ReadAllText($@"Performance\Regression\{testName}.txt");
+            var parsed = new PegParser().Parse(parserSource);
+            var compiled = PegCompiler.Compile(parsed);
+            Assert.That(compiled.Errors.Where(e => !e.IsWarning), Is.Empty);
+            var pegParser = CodeCompiler.Compile<dynamic>(compiled.Code);
+
+            Evaluate(() =>
+            {
+                pegParser.Parse(subject);
+            });
+        }
+    }
+}
